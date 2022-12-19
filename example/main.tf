b8954e56b7e26a33400e5062127099f58a7bfc04
Prashant Gupta <prashant.gupta@tothenew.com>
2022-12-19 18:53:21 +0530

    Added usage and example

--- example/main.tf
+++ example/main.tf
@@ -1,3 +1,9 @@
 module "waf_log_service" {
-    source = "git::https://github.com/prashantgupta123/terraform-aws-waf-monitoring.git"
+    source              = "git::https://github.com/tothenew/terraform-aws-waf-monitoring.git"
+    project_name_prefix = "tothenew"
+    bucket_name         = "aws-waf-logs-tothenew"
+    elastic_host        = "10.20.10.10"
+    subnet_ids          = ["sg-9999999", "sg-9999999"]
+    vpc_id              = "vpc-999999999999"
+    web_acl_name        = "wafv2-acl"
 }
